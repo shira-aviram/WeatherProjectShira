5a1d1ec3fea17e11175912033e66dbb2c6cee29a
shira-aviram <s214124307@gmail.com>
2023-11-14 20:38:27 +0200

    my commit message

---
+++ js_weather/exports.js
@@ -0,0 +1,37 @@
+
+// let weatherImages = {
+//     "Hot": "1.png",
+//    "Warm": "2.png",
+//     "Mild": "3.png",
+//     "Rain": "4.png",
+//     "Cold": "5.png"
+//   };
+ 
+//   export const declareEvents = () => {
+//        let weatherDescription = data.weather[0].description;
+//         let temperature = data.main.temp - 273.15;
+//   let temperatureCondition = getTemperatureCondition(temperature);
+//   let imgTemperature = weatherImages[temperatureCondition];
+//   return imgTemperature;
+//     }; 
+    
+
+//   // let imgWeather = weatherImages[weatherDescription];
+
+ 
+
+
+//   function getTemperatureCondition(temperature) {
+//     if (temperature >= 30) {
+//       return "Hot";
+//     } else if (temperature >= 20) {
+//       return "Warm";
+//     } else if (temperature >= 10) {
+//         return "Mild";
+//     }
+//     else if(temperature >= 0){
+//         return "Rain";
+//     }
+//     else{ return "Cold";}
+
+//     }
